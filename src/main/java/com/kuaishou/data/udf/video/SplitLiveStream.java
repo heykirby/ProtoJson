750b26a3d6193216e573d5d3176a8da964e8d1ee
xiazehui <xiazehui@kuaishou.com>
2019-12-06 14:17:18 +0800

    add split live stream

---
+++ src/main/java/com/kuaishou/data/udf/video/SplitLiveStream.java
@@ -0,0 +1,64 @@
+package com.kuaishou.data.udf.video;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.shaded.org.apache.commons.io.FilenameUtils;
+
+@Description(name = "SplitLiveStream",
+        value = "_FUNC_(url) - parse live play url,rerurn stream id and transcode template.",
+        extended = "The value is returned as an array(string).none template return  [\"stream_id\","
+                + "\"0\"] ,error url rerurn  [\"-1\",\"-1\"]\n"
+                + "Example:\n"
+                + "  > SELECT _FUNC_(url); return [\"stream_id\",\"transcode_template\"]")
+public class SplitLiveStream extends UDF {
+
+    public ArrayList<String> evaluate(Text input) {
+        if (input == null || input.getLength() < 1) {
+            return null;
+        }
+
+        String url = input.toString();
+        ArrayList<String> contacts = new ArrayList<String>();
+        try {
+            URI uri = URI.create(url);
+            String path = uri.getPath();
+            String name = FilenameUtils.getBaseName(path);
+            String[] split = StringUtils.split(name, "_");
+            if (split.length == 1) {
+                System.out.println(name);
+                contacts.add(name);
+                contacts.add("0");
+            } else if (split.length == 2 && split[0].length() >= 11) {
+                contacts.add(split[0]);
+                contacts.add(split[1]);
+            } else if (split.length == 2 && split[0].length() < 11) {
+                contacts.add(name);
+                contacts.add("0");
+            } else if (split.length > 2) {
+                String[] newSplit = Arrays.copyOf(split, split.length - 1);
+                String tmpStream = StringUtils.join(newSplit, "_");
+                if (tmpStream.length() >= 11) {
+                    contacts.add(tmpStream);
+                    contacts.add(split[split.length - 1]);
+                } else {
+                    contacts.add(name);
+                    contacts.add("0");
+                }
+
+            }
+        } catch (Exception e) {
+            contacts.add("-1");
+            contacts.add("-1");
+            return contacts;
+        }
+        return contacts;
+
+    }
+}
