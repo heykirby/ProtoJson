fe0c345cdef80d70ba3d0d33a0f21df9915a8962
xiazehui <xiazehui@kuaishou.com>
2019-12-11 14:49:08 +0800

    add pts parse detail

--- src/test/java/TestPTS.java
+++ src/test/java/TestPTS.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.shaded.com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
 /**
@@ -10,13 +12,18 @@ import org.junit.Test;
  * Created on 2019-09-06
  */
 public class TestPTS {
-    public static ArrayList<Long> evaluate(String str) throws UDFArgumentException {
+
+
+    public static List<ImmutableList<Long>> evaluate(String str, String base) throws UDFArgumentException {
 
         if (str == null || str.length() == 0) {
             throw new UDFArgumentException("must take two arguments");
         }
+        Long baseTimesatmp = Long.valueOf(base);
 
-        ArrayList<Long> ptsList = new ArrayList<>();
+        List<Long> ptsList = new ArrayList<>();
+        List<Long> ptsList111 = new ArrayList<>();
+        List<ImmutableList<Long>> ptsDetail = new ArrayList<>();
         if (str.contains(",")) {
             for (String s : str.split(",")) {
                 try {
@@ -34,31 +41,34 @@ public class TestPTS {
             }
             long s = 0;
             for (int i = 0; i < bytes.length; i += 2) {
-                s = (bytes[i] << 8) | (bytes[i + 1] & 0xff);
+                s += (bytes[i] << 8) | (bytes[i + 1] & 0xff);
                 ptsList.add(s);
+            }
+
+            long l = 0;
+            for (int i = 0; i < bytes.length; i += 2) {
+                l = (bytes[i] << 8) | (bytes[i + 1] & 0xff);
+                ptsList111.add(l);
             }
         }
         System.out.println(ptsList);
+        System.out.println(ptsList111);
         String dur = "";
-        for (int i = 1; i < ptsList.size(); i += 1) {
-            long l = ptsList.get(i) - ptsList.get(i - 1);
-            dur = dur + "," + String.valueOf(l);
+        for (int i = 2; i < ptsList.size(); i += 1) {
+            if (ptsList.get(i) - ptsList.get(i - 2) > 200) {
+                ptsDetail.add(ImmutableList.of(ptsList.get(i - 2) , ptsList.get(i)));
+            }
         }
-        return ptsList;
+        return ptsDetail;
     }
 
     public static void main(String[] args) throws UDFArgumentException {
-        ArrayList<Long>
+
+        List<ImmutableList<Long>>
                 arr =
                 evaluate(
-                        "AAAAygBjAGUAZABkAGQAZQBfAGYAZABkAGMAZQBkAGQAZgBlAGMAZgBhAGMAZABjAGgAZQBkAGIAYABoAGIAZABmAGIAZQBkAGMAYQBmAGIAYwBlAGcAYgBjAGcAYgBnAGMAYQBlAGMAZgBhAGoAXwBmAGUAYQBhAGUAZABmAGMAYgBnAGEAZgBiAGUAZQBkAGUAYgBkAGQAZABmAGQAZgBjAGIAZABpAGMAXgBlAGY");
-        int a=0;
-        for (int i = 1; i < arr.size(); i++) {
-            System.out.println(i + ":" + arr.get(i));
-            if(arr.get(i)+arr.get(i-1)>200){
-                a+=1;
-            }
-        }
-        System.out.println( a);
+                        "AAAAygBjAGUAZABkAGQAZQBfAGYAZABkAGMAZQBkAGQAZgBlAGMAZgBhAGMAZABjAGgAZQBkAGIAYABoAGIAZABmAGIAZQBkAGMAYQBmAGIAYwBlAGcAYgBjAGcAYgBnAGMAYQBlAGMAZgBhAGoAXwBmAGUAYQBhAGUAZABmAGMAYgBnAGEAZgBiAGUAZQBkAGUAYgBkAGQAZABmAGQAZgBjAGIAZABpAGMAXgBlAGY",
+                        "1575882676000");
+        System.out.println(arr);
     }
 }
