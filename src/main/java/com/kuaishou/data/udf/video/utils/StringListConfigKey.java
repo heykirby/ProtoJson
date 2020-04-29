3f4872923033abe70b900e586211366d7bbabc0d
xiazehui <xiazehui@kuaishou.com>
2020-04-29 16:22:42 +0800

    add zk list

--- src/main/java/com/kuaishou/data/udf/video/utils/StringListConfigKey.java
+++ src/main/java/com/kuaishou/data/udf/video/utils/StringListConfigKey.java
@@ -1,38 +1,50 @@
 package com.kuaishou.data.udf.video.utils;
 
 import static com.kuaishou.framework.util.ObjectMapperUtils.fromJSON;
+import static com.kuaishou.framework.util.ObjectMapperUtils.toJSON;
 
 import java.util.Collections;
 import java.util.Set;
 
+import org.apache.curator.utils.ZKPaths;
+
 import com.kuaishou.framework.config.Config;
 
-public enum StringListConfigKey implements Config.ConfigKey<Set<String>> {
-    APP_VERSION_TOP_LIST("appVersionTopList"),
-    APP_VERSION_WHITE_LIST("appVersionWhiteList"),
-    KWAI_CDN_IP_LIST("kwaiCdnIPList"),
-    ;
+public enum StringListConfigKey implements Config.ConfigKey<Set<?>>  {
+    esLogWhiteList(Long.class), //id白名单
+    esLogWhiteListDeviceId(String.class), // device id白名单
+    esLogWhiteListAppVersion(String.class), // 版本白名单
+    kwaiCdnIPList(String.class),
+    appVersionTopList(String.class);
 
-    private String path;
 
-    StringListConfigKey(String path) {
-        this.path = path;
+    private Class<?> targetClass;
+
+    StringListConfigKey(Class<?> targetClass) {
+        this.targetClass = targetClass;
+    }
+
+
+    @Override
+    public Set<?> defaultValue() {
+        return Collections.emptySet();
     }
 
     @Override
     public String configKey() {
-        return "/com/kuaishou/data/udf/video/list/" + this.path;
+        return ZKPaths.makePath("video/list/", name());
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public Set<String> value(String rawValue) throws Exception {
-        Set<String> result = fromJSON(rawValue, Set.class, String.class);
+    public Set<Object> value(String rawValue) throws Exception {
+        Set<Object> result = fromJSON(rawValue, Set.class, (Class<Object>) targetClass);
 
         return Collections.unmodifiableSet(result);
     }
 
     @Override
-    public Set<String> defaultValue() {
-        return Collections.emptySet();
+    public String serialize(Set<?> value) {
+        return toJSON(value);
     }
 }
