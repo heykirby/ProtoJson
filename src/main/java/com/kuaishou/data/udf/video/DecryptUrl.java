95d35ca646ce3ce58e3c5bc22b9a122be9d5f54c
niesipin <niesipin@kuaishou.com>
2020-09-03 10:38:59 +0800

    Catch all exception

--- src/main/java/com/kuaishou/data/udf/video/DecryptUrl.java
+++ src/main/java/com/kuaishou/data/udf/video/DecryptUrl.java
@@ -22,14 +22,15 @@ public class DecryptUrl extends UDF {
                     suffix = originUrl.split(url.getPath())[1];
                 return originUrl.split("/ksc")[0] + realPath + suffix;
             }
-        } catch (URISyntaxException | IllegalArgumentException ignored) {
+        } catch (Exception ignored) {
+            return "invalid_url:" + originUrl;
         }
 
         return originUrl;
     }
 
     public static void main(String[] args) {
-        String url = "http://123.6.12.228/ksc1/TXDQIYF10QIv-kKQ1ryl1ICGLNBjGfQJ8OWlUrhGn-GnVnJgFnGhyNw54-tyEeyGwywuZD71YLUwHRd2ua7lagl9iDv0imyWR*0bxA_MngHh5HE-xB_cFI1yWJjFyw2WRNLEYjaeSTY_NXnr1C9mtv1Kj12FQ7Zo8KrsTpn2IusmcmrBJrVbGZaFVqnZBHEe.mp4?tag=1-1595758298-p-0-5967be10b83044ab-3d4b358efb604372&tt=swft&di=7b0fa029&bp=10081";
+        String url = "httpkwaiprotocol:http://tx-playback.video.yximgs.com/mediacloud/gzone/game_video/segment/Sr3dUn7gHpn7anZnr5s82VeTEbWG1_mZUbi9N31yg_c.m3u8";
         DecryptUrl decryptUrl = new DecryptUrl();
         System.out.println("flag: " + decryptUrl.evaluate(url));
     }
