9fe9389ac25c3511785d7567bbe49aca16c1313c
wuxuexin <wuxuexin@kuaishou.com>
2020-09-15 16:20:01 +0800

    add highlights udf

--- src/main/java/com/kuaishou/data/udf/video/Md5UDF.java
+++ src/main/java/com/kuaishou/data/udf/video/Md5UDF.java
@@ -15,65 +15,65 @@ import org.apache.hadoop.io.Text;
  * 搬运工
  */
 @Description(name = "md5",
-		value = "_FUNC_(str or bin) - Calculates an MD5 128-bit checksum for the string or binary.",
-		extended = "The value is returned as a string of 32 hex digits, or NULL if the argument was NULL.\n"
-				+ "Example:\n"
-				+ "  > SELECT _FUNC_('ABC');\n"
-				+ "  '902fbdd2b1df0c4f70b4a5d23525e932'\n"
-				+ "  > SELECT _FUNC_(binary('ABC'));\n"
-				+ "  '902fbdd2b1df0c4f70b4a5d23525e932'")
-public class Md5UDF extends UDF{
-	private final Text result = new Text();
-	private final MessageDigest digest;
+        value = "_FUNC_(str or bin) - Calculates an MD5 128-bit checksum for the string or binary.",
+        extended = "The value is returned as a string of 32 hex digits, or NULL if the argument was NULL.\n"
+                + "Example:\n"
+                + "  > SELECT _FUNC_('ABC');\n"
+                + "  '902fbdd2b1df0c4f70b4a5d23525e932'\n"
+                + "  > SELECT _FUNC_(binary('ABC'));\n"
+                + "  '902fbdd2b1df0c4f70b4a5d23525e932'")
+public class Md5UDF extends UDF {
+    private final Text result = new Text();
+    private final MessageDigest digest;
 
-	public Md5UDF() {
-		try {
-			digest = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public Md5UDF() {
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * Convert String to md5
-	 */
-	public Text evaluate(Text n) {
-		if (n == null) {
-			return null;
-		}
+    /**
+     * Convert String to md5
+     */
+    public Text evaluate(Text n) {
+        if (n == null) {
+            return null;
+        }
 
-		digest.reset();
-		digest.update(n.getBytes(), 0, n.getLength());
-		byte[] md5Bytes = digest.digest();
-		String md5Hex = Hex.encodeHexString(md5Bytes);
+        digest.reset();
+        digest.update(n.getBytes(), 0, n.getLength());
+        byte[] md5Bytes = digest.digest();
+        String md5Hex = Hex.encodeHexString(md5Bytes);
 
-		result.set(md5Hex);
-		return result;
-	}
+        result.set(md5Hex);
+        return result;
+    }
 
-	/**
-	 * Convert bytes to md5
-	 */
-	public Text evaluate(BytesWritable b) {
-		if (b == null) {
-			return null;
-		}
+    /**
+     * Convert bytes to md5
+     */
+    public Text evaluate(BytesWritable b) {
+        if (b == null) {
+            return null;
+        }
 
-		digest.reset();
-		digest.update(b.getBytes(), 0, b.getLength());
-		byte[] md5Bytes = digest.digest();
-		String md5Hex = Hex.encodeHexString(md5Bytes);
+        digest.reset();
+        digest.update(b.getBytes(), 0, b.getLength());
+        byte[] md5Bytes = digest.digest();
+        String md5Hex = Hex.encodeHexString(md5Bytes);
 
-		result.set(md5Hex);
-		return result;
-	}
+        result.set(md5Hex);
+        return result;
+    }
 
-	public static void main(String[] args) {
-		Md5UDF md5UDF = new Md5UDF();
-		Text str = new Text("A000004F88C2B2");
-		Text str_md5 = new Text("4d94e64325a0d9b10753ed599dbcee96");
-		System.out.println(md5UDF.evaluate(str));
-		System.out.println(md5UDF.evaluate(str).equals(str_md5));
-	}
+    public static void main(String[] args) {
+        Md5UDF md5UDF = new Md5UDF();
+        Text str = new Text("A000004F88C2B2");
+        Text str_md5 = new Text("4d94e64325a0d9b10753ed599dbcee96");
+        System.out.println(md5UDF.evaluate(str));
+        System.out.println(md5UDF.evaluate(str).equals(str_md5));
+    }
 
 }
