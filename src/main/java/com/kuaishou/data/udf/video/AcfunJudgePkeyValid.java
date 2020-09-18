55478ad9574d7abb530ce3886aa787f528d87480
chenxinwei <chenxinwei@kuaishou.com>
2020-09-18 20:29:46 +0800

    test

--- src/main/java/com/kuaishou/data/udf/video/AcfunJudgePkeyValid.java
+++ src/main/java/com/kuaishou/data/udf/video/AcfunJudgePkeyValid.java
@@ -1,5 +1,8 @@
 package com.kuaishou.data.udf.video;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 
@@ -8,13 +11,18 @@ import com.kuaishou.cdn.api.safety.CdnAuthSigner;
 
 public class AcfunJudgePkeyValid extends UDF {
 
-    public boolean evaluate(String url, String domain, Long timestamp) throws UDFArgumentException {
+    public static String evaluate(String url, String domain, Long timestamp) throws UDFArgumentException {
 
         if (url == null || url.length() == 0 || domain == null || domain.length() == 0 || timestamp == null) {
             throw new UDFArgumentException("must take three arguments");
         }
-
-        return CdnAuthSigner
-                .checkAuthInfoForAcFun(url, domain, timestamp);
+        try {
+            return String.valueOf(CdnAuthSigner.checkAuthInfoForAcFun(url, domain, timestamp));
+        }catch (Exception e){
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            return sw.toString();
+        }
     }
 }
