079e43faf569cc8c2ea92bd23408b733327355a8
chenxinwei <chenxinwei@kuaishou.com>
2021-01-07 14:12:44 +0800

    update

--- src/main/java/com/kuaishou/data/udf/video/AcfunJudgePkeyValid.java
+++ src/main/java/com/kuaishou/data/udf/video/AcfunJudgePkeyValid.java
@@ -24,9 +24,7 @@ public class AcfunJudgePkeyValid extends UDF {
     public static void main(String[] args) throws UDFArgumentException {
         String url =
                 "http://tx-safety-video.acfun.cn/mediacloud/acfun/acfun_video/hls/JquYMt2x"
-                        + "-HLjJWAZsqK0jGYm5qVSDU2qB9cATx_9OBaV_xDeQCdB0O2tTFt8kfs3.00009"
-                        + ".ts?pkey"
-                        +
+                        + "-HLjJWAZsqK0jGYm5qVSDU2qB9cATx_9OBaV_xDeQCdB0O2tTFt8kfs3.00009" + ".ts?pkey" +
                         "=AAMuSHrHibH48r5XuqUVuKZ9BK_3Oc_9kADiEa0p9VZqG9VYkBY_55fJWQgcvz5FloNOk_RvT26EfzkJziblbwSijS4mFYcoBWDKPCzoSvfk-e8eiuFD5UD0GW9tQvu14x2Lu6B9qsR81R5P-nS1EptW9zqrtcAFcPkQm01k3wCde8lr7CSJcxL7XOK27C4lNyEG9jPu_VJl4JCJXPRdw71DsX28vhZA8yITwRJrEV__aspeFnWu8FgDG_A7Vs6X5Ep6PR4E9mIS0I5jh_Xq0mA4IVZWMZaWlnSuRa_DfiYKUbU2S5tlAby5sQDQd8TYDx6Xo1MGzMSY1hy31Vw9noqZ";
         String domain = "tx-safety-video.acfun.cn";
         AcfunJudgePkeyValid judgePkeyValid = new AcfunJudgePkeyValid();
