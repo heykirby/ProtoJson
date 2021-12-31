124286cef3392b2869fd58b5724aba6fc2e0b82c
zhouwenjia <zhouwenjia@kuaishou.com>
2021-12-31 10:59:09 +0800

    insert KmeetingCountUDF

--- src/main/java/com/kuaishou/data/udf/video/KMeetingCount.java
+++ src/main/java/com/kuaishou/data/udf/video/KMeetingCount.java
@@ -17,7 +17,6 @@ public class KMeetingCount extends UDF {
         boolean evaluate = KMeetingCount.evaluate("185546807739416936");
         System.out.println(evaluate);
     }
-
     public static boolean evaluate(String id) {
         Set<String> set = Kconfs.ofStringSet("videoData.whiteList.kwai_meeting", new HashSet<>()).build().get();
         return set.contains(id);
