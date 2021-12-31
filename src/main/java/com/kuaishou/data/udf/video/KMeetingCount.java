7178110bb8a3ddef0b39b349c0573926997a0e0b
zhouwenjia <zhouwenjia@kuaishou.com>
2021-12-31 10:25:16 +0800

    第一次提交

---
+++ src/main/java/com/kuaishou/data/udf/video/KMeetingCount.java
@@ -0,0 +1,25 @@
+package com.kuaishou.data.udf.video;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import com.kuaishou.kconf.client.Kconfs;
+
+
+/**
+ * @author yourname <zhouwenjia@kuaishou.com>
+ * Created on 2021-12-30
+ */
+public class KMeetingCount extends UDF {
+    public static void main(String[] args) {
+        boolean evaluate = KMeetingCount.evaluate("185546807739416936");
+        System.out.println("=======开始输出结果==========");
+        System.out.println(evaluate);
+    }
+    public static boolean evaluate(String id) {
+        Set<String> set = Kconfs.ofStringSet("videoData.whiteList.kwai_meeting", new HashSet<>()).build().get();
+        return set.contains(id);
+    }
+}
