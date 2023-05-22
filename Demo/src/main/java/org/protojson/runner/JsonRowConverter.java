df87231b2bb424003ef1a35adffd2893f6d0abd7
“yehe” <yehe@kuaishou.com>
2023-05-22 11:22:30 +0800

    fix

--- Demo/src/main/java/org/protojson/runner/JsonRowConverter.java
+++ Demo/src/main/java/org/protojson/runner/JsonRowConverter.java
@@ -1,11 +1,13 @@
 package org.protojson.runner;
 
 import java.io.IOException;
+import java.util.Stack;
 
 import org.protojson.pojo.JsonNode;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -16,12 +18,19 @@ public class JsonRowConverter {
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private String _json;
     private JsonNode root = new JsonNode("root");
-    private JsonNode _ptr = root;
     private JsonNode[] row;
     private String[] result;
-    private static String[] EMPTY_RESULT;
+    private String[] EMPTY_RESULT;
+    private Stack<JsonToken> stack = new Stack<>();
     private final int NUM;
-    private int currentVersion = 0;
+    private long currentVersion = 0;
+
+    static {
+        // Allows for unescaped ASCII control characters in JSON values
+        MAPPER.enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature());
+        // Enabled to accept quoting of all character backslash qooting mechanism
+        MAPPER.enable(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER.mappedFeature());
+    }
 
     public JsonRowConverter(String... args) {
         NUM = args.length;
@@ -49,133 +58,140 @@ public class JsonRowConverter {
             row[i] = cur;
         }
     }
+
     public String[] process(String json) throws IOException {
         currentVersion++;
-        _ptr = root;
         _json = json;
         if (json == null || json.length() == 0) {
             return EMPTY_RESULT;
         }
         JsonParser parser = MAPPER.createParser(json);
         parser.nextToken();
-        _recursion(_ptr, parser, null);
-        return getResult(_json);
+        _recursion(root, parser);
+        return getResult();
     }
-    private void _recursion(JsonNode ptr, JsonParser parser, JsonToken skipArray) {
-        try {
-            while (parser.currentToken() != null) {
-                //                System.out.println("parser name:" + parser.currentName());
-                //                System.out.println("parser token:" + parser.getCurrentToken().name());
-                //                System.out.println("node name:" + ptr.getName());
-                switch (parser.currentToken()) {
-                    case START_OBJECT:
-                        ptr.setVersion(currentVersion);
-                        ptr.setStart(parser.currentLocation().getColumnNr() - 2);
-                        parser.nextToken();
-                        break;
-                    case END_OBJECT:
-                        ptr.setEnd(parser.getCurrentLocation().getColumnNr() - 1);
-                        ptr = ptr.getParent();
-                        parser.nextToken();
-                        if (skipArray == JsonToken.END_OBJECT) return;
-                        break;
-                    case START_ARRAY:
-                        ptr.setVersion(currentVersion);
-                        ptr.setStart(parser.getCurrentLocation().getColumnNr() - 2);
-                        int i = 0;
-                        while (parser.currentToken() != JsonToken.END_ARRAY) {
-                            parser.nextToken();
-                            if (ptr.getChildren().containsKey("" + i)) {
-                                ptr = ptr.getChildren().get("" + i);
-                                if (parser.currentToken() == JsonToken.START_OBJECT) {
-                                    _recursion(ptr, parser, JsonToken.END_OBJECT);
-                                } else if (parser.currentToken() == JsonToken.START_ARRAY) {
-                                    _recursion(ptr, parser, JsonToken.END_ARRAY);
-                                } else {
-                                    _recursion(ptr, parser, JsonToken.NOT_AVAILABLE);
-                                }
-                                ptr = ptr.getParent();
-                            } else {
-                                skip(parser);
-                            }
-                            ++i;
-                        }
-                        break;
-                    case END_ARRAY:
-                        ptr.setEnd(parser.getCurrentLocation().getColumnNr() - 1);
-                        ptr = ptr.getParent();
-                        parser.nextToken();
-                        if (skipArray == JsonToken.END_ARRAY) return;
-                        break;
-                    case FIELD_NAME:
-                        if (ptr.getChildren() != null && ptr.getChildren().containsKey(parser.getCurrentName())) {
-                            ptr = ptr.getChildren().get(parser.getCurrentName());
-                            ptr.setVersion(currentVersion);
-                            if (ptr.getChildren() == null) {
-                                parser.nextToken();
-                                ptr.setValue(skip(parser,true));
-                                ptr = ptr.getParent();
-                            }
+
+    private void _recursion(JsonNode ptr, JsonParser parser) throws IOException {
+        while (parser.currentToken() != null) {
+            switch (parser.currentToken()) {
+                case START_OBJECT:
+                    ptr.setVersion(currentVersion);
+                    ptr.setStart(parser.currentLocation().getCharOffset() - 1);
+                    stack.push(JsonToken.START_OBJECT);
+                    parser.nextToken();
+                    _recursion(ptr, parser);
+                    return;
+                case END_OBJECT:
+                    ptr.setEnd(parser.getCurrentLocation().getCharOffset());
+                    ptr.setValue(_json.substring((int) ptr.getStart(), (int) ptr.getEnd()));
+                    parser.nextToken();
+                    if (stack.size() <= 0 || stack.peek() != JsonToken.START_OBJECT) {
+                        throw new RuntimeException("parse error");
+                    }
+                    stack.pop();
+                    return;
+                case START_ARRAY:
+                    ptr.setVersion(currentVersion);
+                    ptr.setStart(parser.getCurrentLocation().getCharOffset() - 1);
+                    stack.push(JsonToken.START_ARRAY);
+                    parser.nextToken();
+                    int i = 0;
+                    while (parser.currentToken() != JsonToken.END_ARRAY) {
+                        if (ptr.getChildren().containsKey("" + i)) {
+                            ptr = ptr.getChildren().get("" + i);
+                            _recursion(ptr, parser);
+                            ptr = ptr.getParent();
                         } else {
-                            parser.nextToken();
                             skip(parser);
                         }
-                        parser.nextToken();
-                        break;
-                    case VALUE_STRING:
-                    case VALUE_NUMBER_FLOAT:
-                    case VALUE_NULL:
-                    case VALUE_NUMBER_INT:
-                    case VALUE_TRUE:
-                    case VALUE_FALSE:
+                        ++i;
+                    }
+                    break;
+                case END_ARRAY:
+                    ptr.setEnd(parser.getCurrentLocation().getCharOffset());
+                    if (ptr.isLeaf()) {
+                        ptr.setValue(_json.substring((int) ptr.getStart(), (int) ptr.getEnd()));
+                    }
+                    parser.nextToken();
+                    if (stack.size() <= 0 || stack.peek() != JsonToken.START_ARRAY) {
+                        throw new RuntimeException("parse error");
+                    }
+                    stack.pop();
+                    return;
+                case FIELD_NAME:
+                    parser.nextToken();
+                    if (ptr.getChildren() != null && ptr.getChildren().containsKey(parser.getCurrentName())) {
+                        ptr = ptr.getChildren().get(parser.getCurrentName());
                         ptr.setVersion(currentVersion);
-                        ptr.setValue(parser.getValueAsString());
+                        if (ptr.getChildren() == null) {
+                            ptr.setValue(skip(parser, true));
+                        } else {
+                            _recursion(ptr, parser);
+                        }
                         ptr = ptr.getParent();
-                        if (skipArray == JsonToken.NOT_AVAILABLE) return;
-                        break;
-                }
+                    } else {
+                        skip(parser);
+                    }
+                    break;
+                case VALUE_STRING:
+                case VALUE_NUMBER_FLOAT:
+                case VALUE_NULL:
+                case VALUE_NUMBER_INT:
+                case VALUE_TRUE:
+                case VALUE_FALSE:
+                    ptr.setVersion(currentVersion);
+                    ptr.setValue(parser.getValueAsString());
+                    return;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-    private String[] getResult(String json) {
+
+    private String[] getResult() {
         for (int i = 0; i < NUM; i++) {
             if (row[i].getVersion() < currentVersion) {
                 result[i] = null;
                 continue;
             }
-            if (row[i].getChildren() != null) {
-                result[i] = json.substring(row[i].getStart(), row[i].getEnd());
-            } else {
-                result[i] = row[i].getValue();
-            }
+            result[i] = row[i].getValue();
         }
         return result;
     }
-    private String skip(JsonParser parser, boolean flag) throws IOException {
-        _ptr.setVersion(currentVersion);
-        int i = 0, start;
-        start = flag ? parser.getCurrentLocation().getColumnNr() - 2 : 0;
+
+    private String skip(JsonParser parser, boolean retainValue) throws IOException {
+        int i = 0;
+        long start = retainValue ? parser.getCurrentLocation().getCharOffset() -1 : 0;
+        String result;
         switch (parser.currentToken()) {
             case START_OBJECT:
                 i++;
                 while (i > 0 && parser.nextToken() != null) {
-                    if (parser.currentToken() == JsonToken.START_OBJECT) i++;
-                    else if (parser.currentToken() == JsonToken.END_OBJECT) i--;
+                    if (parser.currentToken() == JsonToken.START_OBJECT) {
+                        i++;
+                    } else if (parser.currentToken() == JsonToken.END_OBJECT) {
+                        i--;
+                    }
                 }
-                return flag ? _json.substring(start, parser.getCurrentLocation().getColumnNr() - 1): null;
+                result = retainValue ? _json.substring((int) start, (int) parser.getCurrentLocation().getCharOffset()) : null;
+                break;
             case START_ARRAY:
                 i++;
                 while (i > 0 && parser.nextToken() != null) {
-                    if (parser.currentToken() == JsonToken.START_ARRAY) i++;
-                    else if (parser.currentToken() == JsonToken.END_ARRAY) i--;
+                    if (parser.currentToken() == JsonToken.START_ARRAY) {
+                        i++;
+                    } else if (parser.currentToken() == JsonToken.END_ARRAY) {
+                        i--;
+                    }
                 }
-                return flag ? _json.substring(start, parser.getCurrentLocation().getColumnNr() - 1): null;
+                result = retainValue ? _json.substring((int) start, (int) parser.getCurrentLocation().getCharOffset()) : null;
+                break;
             default:
-                return flag ? parser.getValueAsString() : null;
+                result = retainValue ? parser.getValueAsString() : null;
+                break;
         }
+        parser.nextToken();
+        return result;
     }
+
     private void skip(JsonParser parser) throws IOException {
         skip(parser, false);
     }
